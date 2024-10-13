2f8fd31c4207d662c956a2a4810f2b9fbae9f4ca
Stefan Stojanovski <stefan.stojanovski@endava.com>
2024-10-13 20:30:25 +0200

    first commit

---
+++ tests/example.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+
+  await page.goto('/');
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  
+  await expect(page.locator('[data-test="title"]')).toBeVisible();
+  await expect(page.locator('[data-test="title"]')).toContainText('Products');
+});
+
